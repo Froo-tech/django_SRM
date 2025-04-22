fdff05d0baaef27737749a4f6e6748ad1e587d91
Frizzi <144421526+Froo-tech@users.noreply.github.com>
2025-04-22 10:30:11 +0700

    Delete SRM directory

--- SRM/SRM/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-app_name = 'users'
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include("users.urls"), name="users"),
-
-]
